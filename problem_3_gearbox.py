49283f5fc00d5109a7481425bdcf33c2a8539a87
Tomaz Canabrava <tcanabrava@kde.org>
2026-05-13 18:49:13 +0200

    Start of gearbox implementation

---
+++ problem_3_gearbox.py
@@ -0,0 +1,22 @@
+
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path",    default="gearbox/*.csv")
+    parser.add_argument("--k_min",        type=int,   default=2)
+    parser.add_argument("--k_max",        type=int,   default=12)
+    parser.add_argument("--top_outliers", type=int,   default=25)
+    parser.add_argument("--viz_sample",   type=float, default=0.0005)
+    parser.add_argument("--output_dir",   default="./output_p3")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+
+if __name__ == "__main__":
+    main()
