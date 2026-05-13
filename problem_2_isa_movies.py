b40358be2b9f56ecacb42286dc2bdd918e275b6f
Tomaz Canabrava <tcanabrava@kde.org>
2026-05-13 12:06:22 +0200

    Prepare the csv schema, and fetch the data

--- problem_2_isa_movies.py
+++ problem_2_isa_movies.py
@@ -3,6 +3,21 @@ import argparse
 from pyspark.sql import SparkSession
 from util import update_nltk_stopwords
 
+from pyspark.sql.types import (StructType, StructField,
+                                IntegerType, StringType)
+
+def movie_csv_schema():
+    return StructType([
+        StructField("release_year", IntegerType(), True),
+        StructField("title",        StringType(),  True),
+        StructField("origin",       StringType(),  True),
+        StructField("director",     StringType(),  True),
+        StructField("cast",         StringType(),  True),
+        StructField("genre",        StringType(),  True),
+        StructField("wiki_page",    StringType(),  True),
+        StructField("plot",         StringType(),  True),
+    ])
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_path", default="movieplots/wiki_movie_plots_deduped.csv")
@@ -16,3 +31,16 @@ def parse_args():
     sc.setLogLevel("WARN")
 
     update_nltk_stopwords()
+
+    df = (spark.read
+          .option("header",    "true")
+          .option("quote",     '"')
+          .option("escape",    '"')
+          .option("multiLine", "true")
+          .schema(movie_csv_schema())
+          .csv(args.data_path))
+
+    df = df.select("title", "genre", "plot").na.drop(subset=["plot"])
+    numDocs = df.count()
+    print(f"Movie articles loaded: {numDocs}")
+    df.show(5, truncate=80)
